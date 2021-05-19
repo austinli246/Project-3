8fa1216b0368f0abd0d5657dc81b9573f01bb231
austin li <austinweili@gmail.com>
2021-05-19 15:40:09 -0400

    added top margin

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -29,7 +29,7 @@ function Game(props){
   
     return (
         <div>
-            <p className = "container text-center"> 
+            <p className = "container mt-5 text-center"> 
                 
                 <div className = "display-1">
                  {game.name} 
